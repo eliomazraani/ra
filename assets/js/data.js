fc9da2d58c5bde6d5a3d90dcd281c80e6ac63b99
eliomazraani <egmazraani@gmail.com>
2023-09-25 14:46:29 +0300

    Modifications

--- assets/js/data.js
+++ assets/js/data.js
@@ -65,6 +65,7 @@ function renderService() {
         $(".services .wide").append(serviceD);
         $(".services .narrow .fader").append(serviceM);
     });
+    console.log("renderService function called");
 }
 
 var values = [
